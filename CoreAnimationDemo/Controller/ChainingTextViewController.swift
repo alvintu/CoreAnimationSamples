4873af23a6d8ead9e4a87f1cca72e30561e7725f
Alvin <tu.alvinj@gmail.com>
2021-03-29 22:56:55 -0400

    Add view controller

---
+++ CoreAnimationDemo/Controller/ChainingTextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChainingTextViewController.swift
+//  CoreAnimationDemo
+//
+//  Created by Alvin Tu on 3/29/21.
+//  Copyright © 2021 Kaibo Lu. All rights reserved.
+//
+
+import UIKit
+
+class ChainingTextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
